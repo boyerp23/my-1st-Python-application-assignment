7142eb40c6683d45a33c2905ab42e4125fb0aa78
boyerp23 <82129609+boyerp23@users.noreply.github.com>
2022-07-24 19:52:46 -0500

    Add files via upload

---
+++ My 1st python application assignment.py
@@ -0,0 +1,50 @@
+#File creator: Paul Boyer
+#pauboyer@uat.edu
+#CSC235 Python Programming 1
+#WK 1 assignment 1
+
+# Block Comments
+"""
+*** Welcome to the Learn Python Because it is Really Cool Tutorial ***
+
+
+In this lesson, we are going to master the "print" command in Python.
+Are you ready?
+What do you think the "print" command in Python is?
+Okay, times up!
+If you thought it was "print" you would be correct!
+----------------------------------------
+Here is the syntax for the "print" command in Python
+print("Text to display on the screen.")
+----------------------------------------
+Here is how you would use the "print" command in Python to print out the phrase "I am the HAL 9000 Computer"
+print("I am the HAL 9000 Computer")
+The following would display in your application
+
+I am the HAL 9000 Computer
+Pro Tip: the print command must be all lower case.
+----------------------------------------
+End of Tutorial... Have a nice day!
+"""
+#end of block comments 
+
+#Calling the print function to display information
+print("*** Welcome to the Learn Python Because it is Really Cool Tutorial ***\n\n")
+print("In this lesson, we are going to master the \"print\" command in Python.\n",\
+        "Are you ready?\n",\
+        "What do you think the \"print\" command in Python is?\n",\
+        "Okay, times up!\n",\
+        "If you thought it was \"print\" you would be correct!\n",\
+        "----------------------------------------\n",\
+        "Here is the syntax for the \"print\" command in Python\n",\
+        "print(\"Text to display on the screen.\")\n",\
+        "----------------------------------------\n"
+        "Here is how you would use the \"print\" command in Python to print out the phrase \"I am the HAL 9000 Computer\"\n",\
+        "print(\"I am the HAL 9000 Computer\")\n",\
+        "The following would display in your application\n\n",\
+
+        "I am the HAL 9000 Computer\n"
+        "Pro Tip: the print command must be all lower case.\n",\
+        "----------------------------------------\n",\
+        "End of Tutorial... Have a nice day!\n")
+# end of program 
